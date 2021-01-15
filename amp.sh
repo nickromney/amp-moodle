e240062294a8d957228e0ab349b9922d62bd4101
Nick Romney <67820132+nickromneydfe@users.noreply.github.com>
2021-01-15 14:10:45 +0000

    apache and refactor

--- amp.sh
+++ amp.sh
@@ -20,12 +20,51 @@ checkPrivileges() {
     fi
 }
 
-updatePackageRepositories() {
+systemPackageAdd() {
+    local packageToAdd="$1"
+    sudo apt install -qy "${packageToAdd}"
+}
+
+systemPackageUpdateRepositories() {
 	echo "Updating package repositories"
 	sudo apt -qq update
 }
 
 # Main functions
+
+apacheInstall() {
+    if ! checkIsCommandAvailable apachectl
+    then
+        echo "Apache is not yet available. Starting installation."
+	    systemPackageAdd apache2
+    fi
+}
+
+apacheConfigureService() {
+	sudo systemctl enable apache2
+    sudo systemctl start apache2
+}
+
+apacheReload() {
+    sudo systemctl reload apache2
+}
+
+apacheRestart() {
+    sudo systemctl restart apache2
+}
+
+apacheStatus() {
+    echo "Apache - get service status"
+    sudo systemctl status apache2
+	echo "Apache - get version"
+	apache2 -V
+    echo "Apache - list loaded/enabled modules"
+    apache2ctl -M
+    echo "Apache - list enabled sites"
+    apachectl -S
+    echo "Apache - check configuration files for errors"
+    apache2ctl -t
+}
 
 phpGetVersion() {
 	# Extract installed PHP version
@@ -36,8 +75,8 @@ phpGetVersion() {
 phpInstall() {
     if ! checkIsCommandAvailable php
     then
-        echo "PHP is not yet available. Starting installation of PHP"
-	    apt -qy install php
+        echo "PHP is not yet available. Starting installation."
+	   systemPackageAdd php
     fi
 }
 
@@ -52,12 +91,13 @@ phpListModules() {
         echo "List all PHP modules installed by package manager"
         dpkg --get-selections | grep -i php
     fi
-
-
 }
 
 checkPrivileges
-updatePackageRepositories
+systemPackageUpdateRepositories
+apacheInstall
+apacheConfigureService
+apacheStatus
 phpInstall
 phpGetVersion
 phpListModules
