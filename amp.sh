f311907e907a86f313aca7024a2f6cc1c4965676
Nick Romney <67820132+nickromneydfe@users.noreply.github.com>
2021-01-16 09:29:08 +0000

    write moodle config

--- amp.sh
+++ amp.sh
@@ -212,9 +212,58 @@ moodleConfigureDirectories() {
 moodleDownloadExtract() {
 	# Download and extract Moodle
 	sudo mkdir -p ${moodleDir}
-	wget -qO - https://download.moodle.org/download.php/stable${moodleVersion}/moodle-latest-${moodleVersion}.tgz | tar zxv -C ${moodleDir} --strip-components 1
+	sudo wget -qO - https://download.moodle.org/download.php/stable${moodleVersion}/moodle-latest-${moodleVersion}.tgz | tar zxv -C ${moodleDir} --strip-components 1
 	sudo chown -R root:${apacheUser} ${moodleDir}
 	sudo chmod -R 0755 ${moodleDir}
+}
+
+moodleWriteConfig() {
+FILE_CONFIG="${moodleDir}/config.php"
+
+cat << EOF > "${FILE_CONFIG}"
+<?php  // Moodle configuration file
+
+unset(\$CFG);
+global \$CFG;
+\$CFG = new stdClass();
+
+\$CFG->dbtype    = 'mysql';
+\$CFG->dblibrary = 'native';
+\$CFG->dbhost    = 'localhost';
+\$CFG->dbname    = 'test_db';
+\$CFG->dbuser    = 'root';
+\$CFG->dbpass    = 'root';
+\$CFG->prefix    = 'mdl_';
+\$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '3306',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+\$CFG->wwwroot   = 'https://${moodleSiteName}';
+\$CFG->dataroot  = '${moodleDataDir}';
+\$CFG->admin     = 'admin';
+
+\$CFG->directorypermissions = 0777;
+EOF
+
+#if memcached_enabled=1
+#Append
+cat << EOF >> "${FILE_CONFIG}"
+\$CFG->session_handler_class = '\core\session\memcached';
+\$CFG->session_memcached_save_path = '${memcachedServer}:11211';
+\$CFG->session_memcached_prefix = 'memc.sess.key.';
+\$CFG->session_memcached_acquire_lock_timeout = 120;
+\$CFG->session_memcached_lock_expire = 7200;
+EOF
+
+cat << EOF >> "${FILE_CONFIG}"
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
+EOF
 }
 
 checkSudoWithoutPasswordEntry
@@ -232,3 +281,4 @@ apacheEnsureFPM
 # Install Database
 moodleConfigureDirectories
 moodleDownloadExtract
+moodleWriteConfig
