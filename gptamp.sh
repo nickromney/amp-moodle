d27676b732a7399906419bc5b2d0bf451308094f
Nick Romney <26225537+nickromney@users.noreply.github.com>
2023-08-12 21:28:13 +0100

    Add PHP extension checking

--- gptamp.sh
+++ gptamp.sh
@@ -49,6 +49,8 @@ moodleDataDir="/home/${moodleUser}/moodledata"
 # Used internally by the script
 USER_REQUIRES_PASSWORD_TO_SUDO='true'
 NON_ROOT_USER='true'
+SUDO=''
+
 
 # helper functions
 check_is_command_available() {
@@ -153,55 +155,84 @@ install_apache() {
 
 # Function to install PHP and required extensions
 install_php() {
-    echo_stdout_verbose "Installing PHP and required extensions..."
 
-    # Alphabetised version of the list from https://docs.moodle.org/310/en/PHP
-    # The ctype extension is required.
-    # The curl extension is required (required for networking and web services).
-    # The dom extension is required.
-    # The gd extension is recommended (required for manipulating images).
-    # The iconv extension is required.
-    # The intl extension is recommended.
-    # The json extension is required.
-    # The mbstring extension is required.
-    # The openssl extension is recommended (required for networking and web services).
-    # The pcre extension is required.
-    # The simplexml extension is required.
-    # The soap extension is recommended (required for web services).
-    # The spl extension is required.
-    # The tokenizer extension is recommended.
-    # The xml extension is required.
-    # The xmlrpc extension is recommended (required for networking and web services).
-    # The zip extension is required.
+    echo_stdout_verbose "Checking PHP configuration..."
 
-    run_command apt-get install -y "php${PHP_VERSION}" "libapache2-mod-php${PHP_VERSION}" \
-        php-ctype \
-        php-curl \
-        php-dom \
-        php-gd \
-        php-iconv \
-        php-intl \
-        php-json \
-        php-mbstring \
-        php-openssl \
-        php-pcre \
-        php-simplexml \
-        php-soap \
-        php-spl \
-        php-tokenizer \
-        php-xml \
-        php-xmlrpc \
-        php-zip
+    # Check PHP configuration
+    run_command php -v
 
-            if [ "${DB_TYPE}" == "pgsql" ]; then
-                # Install php-pgsql extension for PostgreSQL
-                run_command apt-get install -y php-pgsql
-            else
-                # Install php-mysqli extension for MySQL and MariaDB
-                # Note php-mysql is deprecated in PHP 7.0 and removed in PHP 7.2
-                # Note we are not supporting all the different database types that Moodle supports
-                run_command apt-get install -y php-mysqli
-            fi
+    # Get installed extensions and store in a file
+    run_command php -m > installed_extensions.txt
+
+    # List of required extensions
+    declare -a required_extensions=("ctype" "curl" "dom" "gd" "iconv" "intl" "json" "mbstring" "mysqli" "openssl" "pcre" "SimpleXML" "soap" "SPL" "tokenizer" "xml" "xmlrpc" "zip")
+
+    # Check if required extensions are installed
+    for extension in "${required_extensions[@]}"
+    do
+        if ! grep -q -w "$extension" installed_extensions.txt; then
+            echo_stderr "PHP extension $extension is not installed."
+            echo_stderr "Please install the required PHP extensions and try again."
+            exit 1
+        fi
+    done
+
+    echo_stdout_verbose "PHP configuration check completed."
+
+    # If PHP configuration check succeeded, no need to install PHP
+    if [[ $? -eq 0 ]]; then
+        echo_stdout_verbose "Required PHP extensions are already installed. Skipping PHP installation."
+    else
+        # Install PHP and required extensions
+
+        run_command add-apt-repository ppa:ondrej/php
+
+        echo_stdout_verbose "Installing PHP and required extensions..."
+
+        # Alphabetised version of the list from https://docs.moodle.org/310/en/PHP
+        ## The ctype extension is required (provided by common)
+        # The curl extension is required (required for networking and web services).
+        ## The dom extension is required (provided by xml)
+        # The gd extension is recommended (required for manipulating images).
+        ## The iconv extension is required (provided by common)
+        # The intl extension is recommended.
+        # The json extension is required.
+        # The mbstring extension is required.
+        # The openssl extension is recommended (required for networking and web services).
+        ## To use PHP's OpenSSL support you must also compile PHP --with-openssl[=DIR].
+        # The pcre extension is required (The PCRE extension is a core PHP extension, so it is always enabled)
+        ## The SimpleXML extension is required (provided by xml)
+        # The soap extension is recommended (required for web services).
+        ## The SPL extension is required (provided by core)
+        ## The tokenizer extension is recommended (provided by core)
+        # The xml extension is required.
+        # The xmlrpc extension is recommended (required for networking and web services).
+        # The zip extension is required.
+
+        run_command apt install -y --no-install-recommends "php${PHP_VERSION}"
+
+        run_command apt-get install -y "libapache2-mod-php${PHP_VERSION}" \
+            "php${PHP_VERSION}-common" \
+            "php${PHP_VERSION}-curl" \
+            "php${PHP_VERSION}-gd" \
+            "php${PHP_VERSION}-intl" \
+            "php${PHP_VERSION}-json" \
+            "php${PHP_VERSION}-mbstring" \
+            "php${PHP_VERSION}-soap" \
+            "php${PHP_VERSION}-xml" \
+            "php${PHP_VERSION}-xmlrpc" \
+            "php${PHP_VERSION}-zip"
+
+                if [ "${DB_TYPE}" == "pgsql" ]; then
+                    # Install php-pgsql extension for PostgreSQL
+                    run_command apt-get install -y "php${PHP_VERSION}-pgsql"
+                else
+                    # Install php-mysqli extension for MySQL and MariaDB
+                    # Note php-mysql is deprecated in PHP 7.0 and removed in PHP 7.2
+                    # Note we are not supporting all the different database types that Moodle supports
+                    run_command apt-get install -y "php${PHP_VERSION}-mysqli"
+                fi
+      fi
 }
 
 moodle_config_files() {
@@ -264,76 +295,34 @@ moodle_download_extract() {
   # Download and extract Moodle
   echo_stdout_verbose "Downloading and extracting ${moodleArchive}"
   run_command mkdir -p "${moodleDir}"
-  run_command wget -qO - "${moodleArchive}" | tar zx -C "${moodleDir}" --strip-components 1
+  run_command wget -qO "${moodle-latest-${moodleVersion}.tgz}" "${moodleArchive}"
+  run_command tar zx -C "${moodleDir}" --strip-components 1 -f "${moodle-latest-${moodleVersion}.tgz}"
   run_command chown -R root:"${apacheUser}" "${moodleDir}"
   run_command chmod -R 0755 "${moodleDir}"
 }
-
-while getopts ":afmn:p:v:" opt; do
-    case "${opt}" in
-        a) INSTALL_APACHE=true ;;
-        f) INSTALL_FPM=true ;;
-        m) INSTALL_MOODLE=true ;;
-        n) DRY_RUN=true ;;
-        p) INSTALL_PHP=true; PHP_VERSION=${OPTARG} ;;
-        v) MOODLE_VERSION=${OPTARG} ;;
-        \?) echo "Invalid option: -$OPTARG" >&2
-            usage ;;
-    esac
-done
-
-if check_is_true "${VERBOSE}"; then
-    chosen_options=""
-    if $INSTALL_APACHE; then chosen_options+="Install Apache, "; fi
-    if $INSTALL_FPM; then chosen_options+="Install Apache with FPM, "; fi
-    if $INSTALL_MOODLE; then chosen_options+="Install Moodle, "; fi
-    if $DRY_RUN; then chosen_options+="Dry run, "; fi
-    if $INSTALL_PHP; then chosen_options+="Install PHP version $PHP_VERSION, "; fi
-    if [ -n "$MOODLE_VERSION" ]; then chosen_options+="Use Moodle version: $MOODLE_VERSION, "; fi
-    chosen_options="${chosen_options%, }"
-
-    echo_stdout_verbose "Options chosen: $chosen_options"
-fi
-
-
-
 
 usage() {
     echo_stdout "Usage: $0 [options]"
     echo_stdout "Options:"
     echo_stdout "  -a    Install Apache web server"
     echo_stdout "  -f    Install Apache with FPM"
-    echo_stdout "  -m    Install Moodle"
+    echo_stdout "  -m    Install Moodle version (default: ${MOODLE_VERSION})"
     echo_stdout "  -n    Dry run (show commands without executing)"
     echo_stdout "  -p    Install PHP version (default: ${PHP_VERSION})"
-    echo_stdout "  -v    Specify Moodle branch (default: ${MOODLE_VERSION})"
-    echo_stdout "  Note: Options -p and -v require an argument."
+    echo_stdout "  Note: Options -m -p and require an argument."
     exit 1
 }
 
 # Main function
 main() {
 
-    check_user_is_root
-    if check_is_true "${NON_ROOT_USER}"; then
-      check_user_can_sudo_without_password_entry
-      if check_is_true "${USER_REQUIRES_PASSWORD_TO_SUDO}"; then
-        echo_stderr "User requires a password to issue sudo commands. Exiting"
-        echo_stderr "Please re-run the script as root, or having sudo'd with a password"
-        usage
-      else
-        SUDO='sudo '
-        echo_stdout_verbose "User can issue sudo commands without entering a password. Continuing"
-      fi
-    fi
-
-    run_command apt-get update
-
     if $INSTALL_PHP; then
+        run_command apt-get update
         install_php
     fi
 
     if $INSTALL_APACHE; then
+        run_command apt-get update
         install_apache
     fi
 
@@ -343,6 +332,56 @@ main() {
         moodle_config_files
     fi
 }
+
+    # Parse command line options
+    while getopts ":afhm:np:v" opt; do
+        case "${opt}" in
+            a) INSTALL_APACHE=true ;;
+            f) INSTALL_FPM=true ;;
+            h) usage ;;
+            m) INSTALL_MOODLE=true; MOODLE_VERSION=${OPTARG:-"${MOODLE_VERSION}"} ;;
+            n) DRY_RUN=true ;;
+            p) INSTALL_PHP=true; PHP_VERSION=${OPTARG:-"${PHP_VERSION}"} ;;
+            v) VERBOSE=true ;;
+            \?) echo_stderr "Invalid option: -$OPTARG" >&2
+                usage ;;
+            :)
+              echo_stderr "Option -$OPTARG requires an argument."
+              usage
+              ;;
+        esac
+    done
+
+    if check_is_true "${VERBOSE}"; then
+        chosen_options=""
+        if $INSTALL_APACHE; then chosen_options+="Install Apache, "; fi
+        if $INSTALL_FPM; then chosen_options+="Install Apache with FPM, "; fi
+        if $INSTALL_MOODLE; then chosen_options+="Install Moodle version $MOODLE_VERSION, "; fi
+        if $DRY_RUN; then chosen_options+="DRY RUN, "; fi
+        if $INSTALL_PHP; then chosen_options+="Install PHP version $PHP_VERSION, "; fi
+        chosen_options="${chosen_options%, }"
+
+        echo_stdout_verbose "Options chosen: $chosen_options"
+    fi
+
+    # if DRY_RUN, check if user is root
+    if check_is_true "${DRY_RUN}"; then
+        echo_stdout_verbose "DRY RUN: No need to check if user is root."
+        echo_stdout_verbose "DRY RUN: No need to check if user can sudo without password entry."
+    else
+        check_user_is_root
+        if check_is_true "${NON_ROOT_USER}"; then
+          check_user_can_sudo_without_password_entry
+          if check_is_true "${USER_REQUIRES_PASSWORD_TO_SUDO}"; then
+            echo_stderr "User requires a password to issue sudo commands. Exiting"
+            echo_stderr "Please re-run the script as root, or having sudo'd with a password"
+            usage
+          else
+            SUDO='sudo '
+            echo_stdout_verbose "User can issue sudo commands without entering a password. Continuing"
+          fi
+        fi
+    fi
 
 # Run the main function
 main
